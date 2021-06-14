070475b606e2eb238a6c47ee57ed5e65ebe054b6
Victor Dantas <victor_santos9@hotmail.com>
2021-06-13 23:00:29 -0300

    Instala moment

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,6 +32,7 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     { src: '~/plugins/vuex-persist', ssr: false },
+    { src: '~/plugins/moment', ssr: false },
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
